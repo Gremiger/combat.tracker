4c97ed03548d55feec09e1a8fbea6c469eda87ad
Gremiger <sngremiger@gmail.com>
2024-02-28 00:30:58 -0300

    Added an "Add Monster" button.  This button opens a popup with name, AC, Initiative and MAX HP. It will submit only the name

--- src/main/java/com/pibes/dnd/combat/tracker/controller/EncounterController.java
+++ src/main/java/com/pibes/dnd/combat/tracker/controller/EncounterController.java
@@ -2,17 +2,36 @@ package com.pibes.dnd.combat.tracker.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class EncounterController {
 
+    private List<String> characters = new ArrayList<>();
+    private List<String> monsters = new ArrayList<>();
+
     @GetMapping("/dnd")
     public String index(Model model) {
         // Add sample characters and monsters to the model
-        model.addAttribute("characters", "Character 1, Character 2");
-        model.addAttribute("monsters", "Monster 1, Monster 2");
+        model.addAttribute("characters", characters);
+        model.addAttribute("monsters", monsters);
         return "index";
+    }
+
+    @PostMapping("/addCharacter")
+    public String addCharacter() {
+        characters.add("New Character");
+        return "redirect:/dnd";
+    }
+
+    @PostMapping("/addMonster")
+    public String addMonster(@RequestParam("name") String name) {
+        monsters.add(name);
+        // Redirect back to the /dnd page
+        return "redirect:/dnd";
     }
 }
 
