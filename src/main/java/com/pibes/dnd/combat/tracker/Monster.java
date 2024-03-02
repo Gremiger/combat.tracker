4c00d72086fa06f428517536212ddfd8b53d75a6
Gremiger <sngremiger@gmail.com>
2024-03-02 18:05:40 -0300

    Using Monster Class with Lombok.
     It really clears all the boiler-plate code.
      Also, the monster is shown in a prettier way

--- src/main/java/com/pibes/dnd/combat/tracker/Monster.java
+++ src/main/java/com/pibes/dnd/combat/tracker/Monster.java
@@ -1,47 +1,15 @@
 package com.pibes.dnd.combat.tracker;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Monster {
     private String name;
     private int ac;
     private int initiative;
     private int health;
-
-    //Constructor
-    public Monster(String name, int ac, int initiative, int health) {
-        this.name = name;
-        this.ac = ac;
-        this.initiative = initiative;
-        this.health = health;
-    }
-
-    //Getters and setters
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-    public int getAC() {
-        return ac;
-    }
-
-    public void setAC(int ac) {
-        this.ac = ac;
-    }
-    public int getInitiative() {
-        return initiative;
-    }
-
-    public void setInitiative(int initiative) {
-        this.initiative = initiative;
-    }
-
-    public int getHealth() {
-        return health;
-    }
-
-    public void setHealth(int health) {
-        this.health = health;
-    }
 }
