55917dcacf31b24e798ff6f754371f6c01c53522
Gremiger <sngremiger@gmail.com>
2024-05-05 18:46:22 -0300

    light mode is now medieval mode.  Buttons are not from bootstrap anymore.

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,111 +1,140 @@
 document.addEventListener("DOMContentLoaded", function() {
-            // Get a reference to the image element
-            const themeIcon = document.getElementById('theme-icon');
+        // Listen clicks in all the document
+        document.addEventListener('click', function(event) {
+            // Obtain elements with 'modal-content' class
+            const modalsContent = document.querySelectorAll('.modal-content');
 
-            // Check if the theme preference is stored in localStorage
-            const themePreference = localStorage.getItem('themePreference');
+            // Check if the click was inside any modal content
+            let isClickInsideModalContent = Array.from(modalsContent).some(modalContent => {
+                return modalContent.contains(event.target);
+            });
 
-            // Apply the theme based on the stored preference
-            if (themePreference === 'light-theme') {
-                // Apply light theme
-                document.body.classList.add('light-theme');
-                document.body.classList.remove('dark-theme');
-            } else {
-                // Apply dark theme (default)
-                document.body.classList.add('dark-theme');
-                themeIcon.style.filter = 'invert(1)';
-                document.body.classList.remove('light-theme');
+            // Only execute it if the click was outside any modal content (and the click was not in a btn
+            if (!isClickInsideModalContent && isAnyModalOpen() && event.target.className !== 'btn-medieval-primary') {
+                closeAllModals();
             }
-
-            // Toggle theme function
-            function toggleTheme() {
-                const body = document.body;
-                const currentTheme = body.classList.contains('light-theme') ? 'light-theme' : 'dark-theme';
-
-                // Toggle between light and dark themes
-                body.classList.toggle('light-theme');
-                body.classList.toggle('dark-theme');
-
-                // Update the theme preference in localStorage
-                const newTheme = body.classList.contains('light-theme') ? 'light-theme' : 'dark-theme';
-                localStorage.setItem('themePreference', newTheme);
-
-                // Apply or remove the filter based on the new theme
-                themeIcon.style.filter = newTheme === 'dark-theme' ? 'invert(1)' : '';
-            }
-
-            // Add event listener to the theme switcher button
-            const themeSwitcher = document.querySelector('.theme-switcher');
-            themeSwitcher.addEventListener('click', toggleTheme);
         });
 
-        // Function to open a modal
-        function openModal(modalName) {
-            document.getElementById(modalName).style.display = "block";
+        // Get a reference to the image element
+        const themeIcon = document.getElementById('theme-icon');
+
+        // Check if the theme preference is stored in localStorage
+        const themePreference = localStorage.getItem('themePreference');
+
+        // Apply the theme based on the stored preference
+        if (themePreference === 'light-theme') {
+            // Apply light theme
+            document.body.classList.add('light-theme');
+            document.body.classList.remove('dark-theme');
+        } else {
+            // Apply dark theme (default)
+            document.body.classList.add('dark-theme');
+            themeIcon.style.filter = 'invert(1)';
+            document.body.classList.remove('light-theme');
         }
 
-        // Function to close a modal
-        function closeModal(modalName) {
-            document.getElementById(modalName).style.display = "none";
+        // Toggle theme function
+        function toggleTheme() {
+            const body = document.body;
+            const currentTheme = body.classList.contains('light-theme') ? 'light-theme' : 'dark-theme';
+
+            // Toggle between light and dark themes
+            body.classList.toggle('light-theme');
+            body.classList.toggle('dark-theme');
+
+            // Update the theme preference in localStorage
+            const newTheme = body.classList.contains('light-theme') ? 'light-theme' : 'dark-theme';
+            localStorage.setItem('themePreference', newTheme);
+
+            // Apply or remove the filter based on the new theme
+            themeIcon.style.filter = newTheme === 'dark-theme' ? 'invert(1)' : '';
         }
 
-        /**
-        * This function is used to heal a combatant in a game.
-        * It sends a POST request to the '/healCombatant' endpoint with the combatant's ID and the amount to heal.
-        * If the request is successful, it updates the combatant's health in the DOM.
-        * If the request fails, it logs the error message.
-        *
-        * @param {Object} button - The button element that triggered the function.
-        */
-        function healCombatant(button){
-            var parent = button.parentElement;
-            var combatantID = parent.id;
-            var amount = parent.children.dmg_heal.value
-            console.log('healing combatant ' + combatantID + " by: " + amount);
-            axios.post('/healCombatant', 'combatantId=' + encodeURIComponent(combatantID) + '&amount=' + encodeURIComponent(amount), {
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded'
-                    }
-                })
-            .then(function(response) {
-                console.log('Combatant Healed: ', response.data);
-                // Update the temporal health in the DOM
-                var temporalHealthElement = parent.querySelector('.temporal-health');
-                if (temporalHealthElement) {
-                    temporalHealthElement.innerText = response.data.newTemporalHealth;
-                }
-            })
-            .catch(function(error) {
-                console.error('Error in healing: ', error.response.data);
-            });
-        }
+        // Add event listener to the theme switcher button
+        const themeSwitcher = document.querySelector('.theme-switcher');
+        themeSwitcher.addEventListener('click', toggleTheme);
+});
 
-        /**
-        * This function is used to damage a combatant in a game.
-        * It sends a POST request to the '/dmgCombatant' endpoint with the combatant's ID and the amount of damage.
-        * If the request is successful, it updates the combatant's health in the DOM.
-        * If the request fails, it logs the error message.
-        *
-        * @param {Object} button - The button element that triggered the function.
-        */
-        function dmgCombatant(button){
-            const parent = button.parentElement;
-            const combatantID = parent.id;
-            const amount = parent.children.dmg_heal.value;
+// Function to open a modal
+function openModal(modalName) {
+    document.getElementById(modalName).style.display = "block";
+}
 
-            axios.post('/dmgCombatant', `combatantId=${encodeURIComponent(combatantID)}&amount=${encodeURIComponent(amount)}`, {
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded'
-                    }
-                })
-                .then(response => {
-                    console.log('Combatant Damaged: ', response.data);
-                    const temporalHealthElement = parent.querySelector('.temporal-health');
-                    if (temporalHealthElement) {
-                        temporalHealthElement.innerText = response.data.newTemporalHealth;
-                    }
-                })
-                .catch(error => {
-                    console.error('Error in damaging: ', error.response.data);
-                });
+// Function to close a modal
+function closeModal(modalName) {
+    document.getElementById(modalName).style.display = "none";
+}
+
+function isAnyModalOpen() {
+    const modals = document.querySelectorAll('.modal');
+    return Array.from(modals).some(modal => modal.style.display !== 'none');
+}
+
+// Function to close all modals
+function closeAllModals() {
+    var modals = document.querySelectorAll('.modal');
+    modals.forEach(function(modal) {
+        modal.style.display = 'none';
+    });
+}
+
+/**
+* This function is used to heal a combatant in a game.
+* It sends a POST request to the '/healCombatant' endpoint with the combatant's ID and the amount to heal.
+* If the request is successful, it updates the combatant's health in the DOM.
+* If the request fails, it logs the error message.
+*
+* @param {Object} button - The button element that triggered the function.
+*/
+function healCombatant(button){
+    var parent = button.parentElement;
+    var combatantID = parent.id;
+    var amount = parent.children.dmg_heal.value
+    console.log('healing combatant ' + combatantID + " by: " + amount);
+    axios.post('/healCombatant', 'combatantId=' + encodeURIComponent(combatantID) + '&amount=' + encodeURIComponent(amount), {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        })
+    .then(function(response) {
+        console.log('Combatant Healed: ', response.data);
+        // Update the temporal health in the DOM
+        var temporalHealthElement = parent.querySelector('.temporal-health');
+        if (temporalHealthElement) {
+            temporalHealthElement.innerText = response.data.newTemporalHealth;
         }
+    })
+    .catch(function(error) {
+        console.error('Error in healing: ', error.response.data);
+    });
+}
+
+/**
+* This function is used to damage a combatant in a game.
+* It sends a POST request to the '/dmgCombatant' endpoint with the combatant's ID and the amount of damage.
+* If the request is successful, it updates the combatant's health in the DOM.
+* If the request fails, it logs the error message.
+*
+* @param {Object} button - The button element that triggered the function.
+*/
+function dmgCombatant(button){
+    const parent = button.parentElement;
+    const combatantID = parent.id;
+    const amount = parent.children.dmg_heal.value;
+
+    axios.post('/dmgCombatant', `combatantId=${encodeURIComponent(combatantID)}&amount=${encodeURIComponent(amount)}`, {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    })
+    .then(response => {
+        console.log('Combatant Damaged: ', response.data);
+        const temporalHealthElement = parent.querySelector('.temporal-health');
+        if (temporalHealthElement) {
+            temporalHealthElement.innerText = response.data.newTemporalHealth;
+        }
+    })
+    .catch(error => {
+        console.error('Error in damaging: ', error.response.data);
+    });
+}
